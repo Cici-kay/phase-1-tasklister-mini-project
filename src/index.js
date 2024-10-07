71e5305c6e99a04f3d998c100bac7a8826889cd9
Cici-kay <kalechecynthia@gmail.com>
2024-10-07 15:30:46 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  buildToDo(e.target.new_task_description.value);
 });
+const listContainer = document.querySelector('#list');
+
+function buildToDo(todo) {
+  let toBeDone = document.createElement('p');
+  toBeDone.textContent = `${todo} `;
+  listContainer.appendChild(toBeDone);
+
+  let button = document.createElement('button');
+  button.textContent = 'X';
+  toBeDone.appendChild(button);
+  button.addEventListener('click', () => {
+    toBeDone.remove();
+  });
+}
